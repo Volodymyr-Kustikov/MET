9db2b738688a7dbd36233bd3e9edded88a1ba68b
Volodymyr Kustikov <115656488+Volodymyr-Kustikov@users.noreply.github.com>
2023-06-13 12:52:32 +0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,9 @@
+'use strickt';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
